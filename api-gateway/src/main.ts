0148dc6be514965e134d07169e6bec069e774fc2
terlanjurtampan36 <daniel.kristi09@gmail.com>
2025-10-08 11:53:25 +0700

    update

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -3,7 +3,20 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'http://localhost:5173' })
+  const defaultOrigins = [
+    'http://localhost:5173',
+    'https://employee-absent-api-gateway.vercel.app'
+  ];
+
+  const allowedOrigins = process.env.CORS_ORIGIN
+    ? process.env.CORS_ORIGIN.split(',')
+    : defaultOrigins;
+
+  app.enableCors({
+    origin: allowedOrigins,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  })
   await app.listen(process.env.PORT ?? 4000);
   console.log('API gateway starting on http://localhost:4000')
 }
