18921a958b8a8dd4062ca962c9ee1b3d67041de7
terlanjurtampan36 <daniel.kristi09@gmail.com>
2025-10-08 12:47:51 +0700

    uop

--- attendance-microservice/src/main.ts
+++ attendance-microservice/src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  const expressApp = app.getHttpAdapter().getInstance();
+  expressApp.set('trust proxy', 1);
   const port = process.env.PORT || 3002
   await app.listen(port);
   console.log(`Attendance Service berjalan di port ${port}`);
--- user-microservice/src/main.ts
+++ user-microservice/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  const expressApp = app.getHttpAdapter().getInstance();
+  expressApp.set('trust proxy', 1);
   const port = process.env.PORT || 3001
   await app.listen(port);
   console.log(`User Service berjalan di port ${port}`);
